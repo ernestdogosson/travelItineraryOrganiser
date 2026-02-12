05e9870903beb396e9ebd2cc7d6f4193d306cf7c
alva-projects <alva.lundberg@hotmail.se>
2026-02-12 15:33:06 +0100

    addActivity() done

--- src/services/activityService.ts
+++ src/services/activityService.ts
@@ -1,1 +1,26 @@
 import { Activity } from "../models";
+import { randomUUID } from "crypto";
+
+let activities: Activity[] = [];
+
+const addActivity = (
+    name: string, 
+    cost: number, 
+    category: "food" | "transport" | "sightseeing", 
+    startTime: Date 
+): void => {
+
+    const newActivity = {
+        id: randomUUID(),
+        name: name,
+        cost: cost,
+        category: category,
+        startTime: startTime,
+    }
+    activities.push(newActivity)
+}
+
+//addActivity();
+//getActivitiesByDay();
+//getActivitiesByCategory();
+//getAllCategoriesChronologically
